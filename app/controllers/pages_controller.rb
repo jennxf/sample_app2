9e13f5d7b8b7714515afb190c834c1be93255ec4
jennxf <jennxf.ng@gmail.com>
2014-04-20 23:08:18 -0700

    added a title helper and style sheets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,17 @@
 class PagesController < ApplicationController 
 
   def home
-  	# render :layout => false
+  	
   	@title = "Home"
   end
 
   def contact
-  	# render :layout => false
+  	
   	@title = "Contact"
   end
 
   def about
-  	# render :layout => false
+  	
   	@title = "About"
   end
 end
